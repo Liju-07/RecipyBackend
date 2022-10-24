3278f4eca97aebef516261c26b62ada98bfed733
Liju-07 <lijusaju03@gmail.com>
2022-10-24 20:22:48 +0530

    food

---
+++ src/test/java/com/nestdigital/Recipybackend/RecipyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.Recipybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
